2436d786f45aad478d34c8b1e3da476a9b1252f6
SolomiyaSvitla <somethingelse1627@gmail.com>
2015-07-27 23:46:14 +0300

    Added migration folder

---
+++ spring-security-custom-login-form-xml/migration/CreateUserTable.sql
@@ -0,0 +1,32 @@
+USE [Tennis]
+GO
+
+/****** Object:  Table [dbo].[Users]    Script Date: 07/27/2015 23:43:50 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Users](
+	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[UserName] [nvarchar](200) NOT NULL,
+	[Password] [nvarchar](200) NOT NULL,
+	[EmailAddress] [nvarchar](200) NOT NULL,
+	[CreateDate] [datetime] NULL,
+ CONSTRAINT [PK_User_UserID] PRIMARY KEY CLUSTERED 
+(
+	[UserID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY],
+ CONSTRAINT [UQ_Users] UNIQUE NONCLUSTERED 
+(
+	[UserName] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[Users] ADD  DEFAULT (getdate()) FOR [CreateDate]
+GO
+
+
