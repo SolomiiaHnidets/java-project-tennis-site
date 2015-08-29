ac4d0973de6524ba5078204f0ed005e709e5c065
yurii-ploskyi-symphony <yurii.ploskyi@symphony-solutions.eu>
2015-08-29 20:20:28 +0300

    Code refactor. Added log4j dependency

--- src/main/java/com/tennis/web/UserController.java
+++ src/main/java/com/tennis/web/UserController.java
@@ -1,73 +1,21 @@
 package com.tennis.web;
 
-import com.tennis.domain.User;
-import com.tennis.persistent.UserDAOjdbcImpl;
-
-import org.springframework.http.HttpHeaders;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/user")
 public class UserController {
 
-	@RequestMapping(value = "/rest", method = RequestMethod.GET)
-	public ResponseEntity<String> rest() {
-		System.out.println("simplePattern method was called");
+	private static final Logger logger = Logger.getLogger(UserController.class);
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<String> getAll() {
+		logger.info("Calling user controller");
 		return new ResponseEntity<String>("Some data", HttpStatus.OK);
-	}
-
-	@RequestMapping("/responseentity")
-	public ResponseEntity<String> handleResponseEntity() {
-
-		HttpHeaders responseHeaders = new HttpHeaders();
-		responseHeaders.set("HeaderKey", "HeaderData");
-		return new ResponseEntity<String>(
-				"<i>This is</i> the <h2>Page value</h2> (ResponseBody)",
-				responseHeaders, HttpStatus.CREATED);
-	}
-	@RequestMapping(value = {"/", "/welcome**"}, method = RequestMethod.GET)
-	public ModelAndView welcomePage() {
-		ModelAndView model = new ModelAndView();
-		model.addObject("title", "Spring Security Custom Login Form");
-		model.addObject("message", "This is welcome page!");
-		model.setViewName("hello");
-		return model;
-	}
-
-	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
-	public ModelAndView adminPage() {
-
-		ModelAndView model = new ModelAndView();
-		model.addObject("title", "Spring Security Custom Login Form");
-		model.addObject("message", "This is protected page!");
-		model.setViewName("admin");
-		return model;
-	}
-
-	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public ModelAndView login(
-			@RequestParam(value = "error", required = false) String error,
-			@RequestParam(value = "logout", required = false) String logout) {
-		ModelAndView model = new ModelAndView();
-		if (error != null) {
-			model.addObject("error", "Invalid username and password!");
-		}
-		if (logout != null) {
-			model.addObject("msg", "You've been logged out successfully.");
-		}
-		model.setViewName("login");
-		return model;
-	}
-
-	@RequestMapping(value = "/show", method = RequestMethod.GET)
-	public ResponseEntity<String> show(
-			@RequestParam(value = "id", required = true) int id) {
-		UserDAOjdbcImpl userDAO = new UserDAOjdbcImpl();
-		User user = userDAO.getById(id);
-		return new ResponseEntity<String>(user.getUserName(), HttpStatus.OK);
 	}
 
 }
