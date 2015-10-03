b6cdc8f27b868f2bce467b1907c434836baa9bbd
SolomiyaSvitla <somethingelse1627@gmail.com>
2015-10-03 16:32:48 +0300

    Used mocks in UserDaoTest

--- persistence/src/main/java/com/tennis/persistence/UserHibernateImpl.java
+++ persistence/src/main/java/com/tennis/persistence/UserHibernateImpl.java
@@ -74,37 +74,37 @@ public class UserHibernateImpl extends AbstractHibernateDaoImpl
 		return null;
 	}
 
-	public static void main(String[] args) {
-		UserDAO userDAO = new UserHibernateImpl();
-		User user = new User("solya", "some");
-		// user.setUserID(3);
-		user.setBirthDate("768976");
-		user.setEmail("ghjk");
-		user.setSex('F');
-		userDAO.create(user);
-		user = new User("yura", "some-some");
-		// user.setUserID(1);
-		user.setBirthDate("08.09.1994");
-		user.setEmail("ghjk");
-		user.setSex('M');
-		userDAO.create(user);
-		user = new User("vasya", "some-some");
-		user.setBirthDate("08.09.1994");
-		user.setEmail("ghjk");
-		user.setSex('M');
-		userDAO.create(user);
-		List<User> users = userDAO.getAll();
-		for (int i = 0; i < users.size(); i++) {
-			System.out.println(users.get(i).getUserName() + " "
-					+ users.get(i).getUserID());
-		}
-		user = userDAO.getById(1);
-		System.out.println(user.getUserName());
-		userDAO.delete(1);
-		users = userDAO.getAll();
-		for (int i = 0; i < users.size(); i++) {
-			System.out.println(users.get(i).getUserName() + " "
-					+ users.get(i).getUserID());
-		}
-	}
+	// public static void main(String[] args) {
+	// UserDAO userDAO = new UserHibernateImpl();
+	// User user = new User("solya", "some");
+	// // user.setUserID(3);
+	// user.setBirthDate("768976");
+	// user.setEmail("ghjk");
+	// user.setSex('F');
+	// userDAO.create(user);
+	// user = new User("yura", "some-some");
+	// // user.setUserID(1);
+	// user.setBirthDate("08.09.1994");
+	// user.setEmail("ghjk");
+	// user.setSex('M');
+	// userDAO.create(user);
+	// user = new User("vasya", "some-some");
+	// user.setBirthDate("08.09.1994");
+	// user.setEmail("ghjk");
+	// user.setSex('M');
+	// userDAO.create(user);
+	// List<User> users = userDAO.getAll();
+	// for (int i = 0; i < users.size(); i++) {
+	// System.out.println(users.get(i).getUserName() + " "
+	// + users.get(i).getUserID());
+	// }
+	// user = userDAO.getById(1);
+	// System.out.println(user.getUserName());
+	// userDAO.delete(1);
+	// users = userDAO.getAll();
+	// for (int i = 0; i < users.size(); i++) {
+	// System.out.println(users.get(i).getUserName() + " "
+	// + users.get(i).getUserID());
+	// }
+	// }
 }
--- persistence/src/test/java/com/tennis/UserDaoTest.java
+++ persistence/src/test/java/com/tennis/UserDaoTest.java
@@ -1,47 +1,103 @@
-//package com.tennis;
-//
-//import com.tennis.domain.User;
-//import com.tennis.configuration.Config;
-//import com.tennis.persistence.UserDAO;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.validation.BindingResult;
-//
-//import static org.mockito.Mockito.*;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = Config.class)
-//public class UserDaoTest {
-//
-//	@Autowired
-//	private UserDAO userDAO;
-//
-//	@Test
-//	public void testGetById() {
-//		User user = userDAO.getById(12);
-//		System.out.println(user);
-//	}
-//
-//	@Test
-//	public void testInsertUser() {
-//		User user = new User();
-//		BindingResult result = mock(BindingResult.class);
-//		user.setUserName("uranfgh21");
-//		user.setPassword("passwfgord");
-//		user.setEmail("some@www.ru");
-//		// Check validation errors
-//		if (result.hasErrors()) {
-//			System.out.println("addUser");
-//		} else {
-//			userDAO.create(user);
-//		}
-//	}
-//
-//	@Test
-//	public void testDeleteUser() {
-//		userDAO.delete(1);
-//	}
-// }
+package com.tennis;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.tennis.domain.User;
+import com.tennis.configuration.Config;
+import com.tennis.persistence.UserDAO;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Config.class)
+public class UserDaoTest {
+
+	@Autowired
+	@InjectMocks
+	private UserDAO userDAO;
+
+	@Mock
+	private Connection connection;
+
+	@Mock
+	private PreparedStatement statement;
+
+	@Mock
+	private ResultSet result;
+
+	@Mock
+	private User user;
+
+	@Before
+	public void init() throws SQLException {
+		MockitoAnnotations.initMocks(this);
+		Mockito.when(connection.createStatement()).thenReturn(statement);
+		user = new User("uranfgh21", "passwfgord");
+	}
+
+	@Test
+	public void testGetById() {
+		try {
+			Mockito.when(
+					connection
+							.prepareStatement("select userName from Users where userID = ? "))
+					.thenReturn(statement);
+			Mockito.when(statement.executeQuery()).thenReturn(result);
+			Mockito.when(result.next()).thenReturn(true);
+			Mockito.when(result.getInt("userID")).thenReturn(1);
+			user = userDAO.getById(1);
+			assertEquals(1, user.getUserID());
+			Mockito.verify(statement).executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// User user = userDAO.getById(12);
+		// System.out.println(user);
+	}
+
+	@Test
+	public void testInsertUser() {
+		try {
+			Mockito.when(
+					connection
+							.prepareStatement("insert into Users (userName, password, email, birthdate, sex) VALUES ( ?, ?, ?, ?, ? )"))
+					.thenReturn(statement);
+			userDAO.create(user);
+			Mockito.verify(statement).executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//
+		// User user = new User();
+		// BindingResult result = mock(BindingResult.class);
+		// user.setUserName("uranfgh21");
+		// user.setPassword("passwfgord");
+		// user.setEmail("some@www.ru");
+		// // Check validation errors
+		// if (result.hasErrors()) {
+		// System.out.println("addUser");
+		// } else {
+		// userDAO.create(user);
+		// }
+	}
+
+	@Test
+	public void testDeleteUser() {
+		userDAO.delete(1);
+	}
+}
